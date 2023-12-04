b7f0e6fe4b8f27d30e3fbf1e98af6ad15e8830d9
Luca Terrazzan <luca.terraz@gmail.com>
2023-12-04 22:53:59 +0100

    parse games

--- 2023/2/part1.js
+++ 2023/2/part1.js
@@ -1,3 +1,43 @@
 import { parseInputFile } from '../utils.js';
 
-parseInputFile('./input_small.txt', line => console.log(line), line => console.log('closed'))
+const MAX_R = 12
+const MAX_G = 13
+const MAX_B = 14
+let totalPoints = 0
+
+const mapCubeNameToLimit = {
+  'red': MAX_R,
+  'green': MAX_G,
+  'blue': MAX_B
+}
+
+function parseGame(gameTextLine) {
+  totalPoints += getPointsFromGame(gameTextLine)
+}
+
+function getPointsFromGame(gameTextLine) {
+  const [gameIdText, gamesText] = gameTextLine.split(':')
+  const [_, gameId] = gameIdText.split(' ')
+  const cubeSets = gamesText.split(';')
+  for (const cubeSet of cubeSets) {
+    if (!isCubesetValid(cubeSet)) {
+      return 0
+    }
+  }
+
+  return +gameId
+}
+
+function isCubesetValid(cubeSetString) {
+  const cubes = cubeSetString.split(', ')
+  for (const cubeTypeAndAmount of cubes) {
+    const [amount, type] = cubeTypeAndAmount.split(' ')
+    if (mapCubeNameToLimit[type] < amount) return false
+  }
+
+  return true
+}
+
+// parseInputFile('./input_small.txt', parseGame, line => console.log(totalPoints))
+// parseInputFile('./sample_input.txt', parseGame, line => console.log(totalPoints))
+parseInputFile('./input.txt', parseGame, line => console.log(totalPoints))

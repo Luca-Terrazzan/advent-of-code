b8430c0e60b0754e2b3049be3ba04022ff536484
Luca Terrazzan <luca.terraz@gmail.com>
2023-12-05 16:54:35 +0100

    fromatting

--- 2023/3/part1.js
+++ 2023/3/part1.js
@@ -1,8 +1,6 @@
 
 import { isNumber, parseInputFile } from '../utils.js';
 
-let totalPoints = 0
-
 const SPACE_CHAR = '.'
 const GAME_MATRIX = []
 
@@ -15,37 +13,37 @@ function addInputLineToGameMatrix(line) {
 }
 
 function parseGame() {
+  let totalPoints = 0
   for (let lineNumber = 0; lineNumber < GAME_MATRIX.length; lineNumber++) {
     const line = GAME_MATRIX[lineNumber]
     for (let charNumber = 0; charNumber < GAME_MATRIX[0].length; charNumber++) {
       const char = line[charNumber]
       if (char === SPACE_CHAR) continue
       if (isNumber(char)) {
-        // console.log('Char ', char, ' is a number')
+        continue
       } else {
-        console.log('Char ', char, ' is a symbol')
-        findNumbersAdjacentToSymbol(lineNumber, charNumber)
+        totalPoints += +findNumbersAdjacentToSymbol(lineNumber, charNumber)
       }
     }
   }
 
-  console.log('Total points: ')
+  console.log('Total points: ', totalPoints)
 }
 
 function findNumbersAdjacentToSymbol(symLine, symCol) {
-  const symbol = GAME_MATRIX[symLine][symCol]
-  console.log('Scanning around symbol ', symbol, ' at pos: ', symLine, symCol)
+  let adjacentNumbersSum = 0
   // Look for the eight spaces around the symbol
   for (let i = Math.max(0, symLine-1); i < Math.min(GAME_MATRIX.length, symLine+2); i++) {
     for (let j = Math.max(0, symCol-1); j < Math.min(GAME_MATRIX[0].length, symCol+2); j++) {
       const char = GAME_MATRIX[i][j]
-      console.log(' at pos: ', i, j, char, ' is it a number?', isNumber(char))
       if (isNumber(char)) {
         const extractedNumber = extractNumberFromPosition(i, j)
-        console.log(' Found adjacent number: ', extractedNumber)
+        adjacentNumbersSum += +extractedNumber
       }
     }
   }
+
+  return adjacentNumbersSum
 }
 
 function extractNumberFromPosition(numLine, numCol) {
@@ -74,5 +72,5 @@ function extractNumberFromPosition(numLine, numCol) {
   return finalNumber
 }
 
-parseInputFile('./2023/3/sample_input.txt', addInputLineToGameMatrix, parseGame)
-// parseInputFile('./2023/3/input.txt', parseGame, line => console.log('Total points: ', totalPoints))
+// parseInputFile('./2023/3/sample_input.txt', addInputLineToGameMatrix, parseGame)
+parseInputFile('./2023/3/input.txt', addInputLineToGameMatrix, parseGame)

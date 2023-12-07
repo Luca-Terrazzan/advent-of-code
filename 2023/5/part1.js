26637ae3f1b4b2ea198bcb88cdfdc29660d05bbb
Luca Terrazzan <luca.terraz@gmail.com>
2023-12-07 11:49:26 +0100

    lazy

--- 2023/5/part1.js
+++ 2023/5/part1.js
@@ -2,15 +2,17 @@ import { parseInputFile } from '../utils.js';
 
 let currentSeeds;
 let currentStep = 'seed'
+let stepMap = {}
 
 function parseInitialSeeds(seedsLine) {
   const [_, seedsListAsString] = seedsLine.split(': ')
-  currentSeeds = seedsListAsString.split(' ')
+  currentSeeds = seedsListAsString.split` `.map(num => +num)
 }
 
 function parseGameLine(line) {
   if (!line) {
-    // ignore empty lines
+    // Empty line, a mapping has finished, update seeds
+    updateSeeds()
     return
   }
   if (line.startsWith('seeds:')) {
@@ -20,13 +22,33 @@ function parseGameLine(line) {
   }
   if (line.startsWith(currentStep)) {
     // a map line header, get the next step
-    line.replace(' map:', '')
-    line.replace(currentStep+'-to-')
-    currentStep = line
+    // parsing the current step name is pretty much useless, still... 🤷
+    const previousStep = currentStep
+    currentStep = line.replace(currentStep, '')
+    currentStep = line.replace(' map:', '')
+    currentStep = currentStep.replace(previousStep+'-to-', '')
+    console.log('Current step is', currentStep)
     return
   }
   // otherwise, we are reading a map numbers list for the current step
+  parseMappingLine(line)
 }
 
-// parseInputFile('./2023/5/input.txt', parseGameLine, line => console.log('Total points: ', totalPoints))
-parseInputFile('./2023/5/sample_input.txt', parseGameLine, line => console.log('Test', currentSeeds))
+function parseMappingLine(line) {
+  const [seedEnd, seedStart, step] = line.split` `.map(x => +x)
+  for (let i = 0; i < step; i++) {
+    stepMap[seedStart + i] = seedEnd + i
+  }
+}
+
+function updateSeeds() {
+  for (let i = 0; i < currentSeeds.length; i++) {
+    const seed = currentSeeds[i]
+    currentSeeds[i] = stepMap[seed] || seed
+  }
+
+  stepMap = {}
+}
+
+parseInputFile('./2023/5/input.txt', parseGameLine, line => console.log('Total points: ', totalPoints))
+// parseInputFile('./2023/5/sample_input.txt', parseGameLine, line => console.log('Lowest final position is', Math.min(...currentSeeds)))
